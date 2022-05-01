0f6edecd2e42a436519bcea21b838a74208cafb2
Martin S <35738176+skrixx68@users.noreply.github.com>
2022-05-01 18:17:32 +0800

    part 5

--- src/anteikupos/FXMLDocumentController.java
+++ src/anteikupos/FXMLDocumentController.java
@@ -14,16 +14,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 
 /**
@@ -60,6 +65,7 @@ public class FXMLDocumentController implements Initializable {
     ResultSet resultSet = null;
     
     ArrayList<Products> products = new ArrayList<Products>();
+    private int selectedIndex = -1;
     
     private void LoadProducts(){
         try{
@@ -72,7 +78,6 @@ public class FXMLDocumentController implements Initializable {
                 float itempriceMedium = resultSet.getFloat("itemprice_Medium");
                 float itempriceLarge = resultSet.getFloat("itemprice_Large");
                 Products product = new Products(productID,productName,itempriceSmall,itempriceMedium,itempriceLarge);
-                System.out.println(productName);
                 products.add(product);
             }
             stmt.close();
@@ -97,11 +102,34 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void addItem(ActionEvent event) {
+        String productID = txtProductCode.getText();
+        String productName = txtProductName.getText();
+        String productSize = productSizes.getSelectionModel().getSelectedItem();
+        Integer prodQuantity = productQuantity.getValue();
+        Float productTotal = Float.parseFloat(txtProductPrice.getText()) * prodQuantity;
+        
+        ObservableList<TableViewModel> data = Prodtbl.getItems();
+        data.add(new TableViewModel(productID,productName,productSize,prodQuantity,productTotal));
+        
+        txtProductCode.clear();
+        txtProductName.clear();
+        productQuantity.getValueFactory().setValue(1);
+        txtProductPrice.clear();
     }
 
     @FXML
     private void removeItem(ActionEvent event) {
-        
+        if(selectedIndex >= 0){
+            ObservableList<TableViewModel> data = Prodtbl.getItems();
+            data.remove(selectedIndex);
+            selectedIndex = -1;
+            Dialog d = new Alert(Alert.AlertType.INFORMATION,"Successfully Updated!");
+            d.show();
+            
+        }else{
+            Dialog d = new Alert(Alert.AlertType.ERROR,"Please Select an Item from the List");
+            d.show();
+        }
     }
 
     @FXML
@@ -112,6 +140,46 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void newTransaction(ActionEvent event) {
         
+    }
+    
+    private void UpdateItemView(){
+        txtProductName.clear();
+        productQuantity.getValueFactory().setValue(1);
+        txtProductPrice.clear();
+        for(Products i: products){
+            if(i.getProductID().equals(txtProductCode.getText())){
+                txtProductName.setText(i.getProductName());
+                float price = 0f;
+                switch(productSizes.getValue()){
+                    case "Small":
+                        price = i.getItemPrice_Small();
+                        break;
+                    case "Medium":
+                        price = i.getItemPrice_Medium();
+                        break;
+                    case "Large":
+                        price = i.getItemPrice_Large();
+                        break;
+                }
+                txtProductPrice.setText(String.valueOf(price));
+            }
+        
+        }
+    }
+
+    @FXML
+    private void updateProductInfo(KeyEvent event) {
+        UpdateItemView();
+    }
+
+    @FXML
+    private void onSizeChanged(ActionEvent event) {
+        UpdateItemView();
+    }
+
+    @FXML
+    private void updateTableViewIndex(MouseEvent event) {
+        selectedIndex = Prodtbl.getSelectionModel().getSelectedIndex();
     }
     
 }
