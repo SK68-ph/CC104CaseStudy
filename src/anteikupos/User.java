9a040145ed3e7fe7f6eee6b79f679a0abf760a0e
Martin S <35738176+skrixx68@users.noreply.github.com>
2022-05-05 00:45:09 +0800

    fix authorization

---
+++ src/anteikupos/User.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anteikupos;
+
+/**
+ *
+ * @author Admin
+ */
+public class User {
+    private String _username;
+    private String _fullname;
+    private user_role _role;
+    
+    
+    public enum user_role{
+        admin,
+        manager,
+        cashier
+    }
+    public User(){}
+    public User(String username,String fullname){
+        setUsername(username);
+        setFullname(fullname);
+    }
+    
+    
+    public String getUsername() { return this._username;}
+    public String getFullname() { return this._fullname;}
+    public user_role getUserRole() { return this._role;}
+    
+    public void setUsername(String username) { this._username = (username);}
+    public void setFullname(String fullname) { this._fullname = (fullname);}
+    public void setUseRole(int role) { 
+        switch(role){
+            case 1:
+                this._role = user_role.admin;
+                break;
+            case 2:
+                this._role = user_role.manager;
+                break;
+            case 3:
+                this._role = user_role.cashier;
+                break;
+            default:
+                this._role = user_role.cashier;
+                break;
+        }
+    }
+}
